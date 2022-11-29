ec875e4900532c0d7a34eab244619c04804c03e2
AbrRahman <abrahman.est@gmail.com>
2022-11-29 16:56:22 +0600

    set authcontext

--- src/Pages/Register/Register.js
+++ src/Pages/Register/Register.js
@@ -1,45 +1,49 @@
 import React from 'react';
-
+import { useForm } from "react-hook-form";
 const Register = () => {
+    const { register, handleSubmit } = useForm();
+    const handelEmailPasswordRegister = (data) => {
+        console.log(data)
+    }
     return (
         <div>
             <div className="hero bg-base-200 ">
                 <div className="hero-content flex-col lg:flex-row-reverse">
                     <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
                         <div className="card-body">
-                            <form>
+                            <form onSubmit={handleSubmit(handelEmailPasswordRegister)}>
                                 <div className="form-control">
                                     <label className="label">
                                         <span className="label-text">Enter Name</span>
                                     </label>
-                                    <input type="text" placeholder="Enter Name" className="input input-bordered" />
+                                    <input type="text" {...register("name")} placeholder="Enter Name" className="input input-bordered" />
                                 </div>
                                 <div className="form-control">
                                     <label className="label">
                                         <span className="label-text">Email</span>
                                     </label>
-                                    <input type="email" placeholder="Enter Email" className="input input-bordered" />
+                                    <input type="email" {...register("email")} placeholder="Enter Email" className="input input-bordered" />
                                 </div>
                                 <div className="form-control">
                                     <label className="label">
                                         <span className="label-text"></span>
                                     </label>
-                                    <input type="file" placeholder="Choose Your" className="input input-bordered" />
+                                    <input type="file" {...register("userPhoto")} placeholder="Choose Your" className="input input-bordered" />
                                 </div>
                                 <div className="form-control w-full max-w-xs">
                                     <label className="label">
                                         <span className="label-text-alt">Select Option</span>
                                     </label>
-                                    <select className="select select-bordered">
-                                        <option value='user' selected>User</option>
-                                        <option value='Seller'>seller</option>
+                                    <select {...register("userRule")} className="select select-bordered">
+                                        <option value='user'>User</option>
+                                        <option value='seller'>Seller</option>
                                     </select>
                                 </div>
                                 <div className="form-control">
                                     <label className="label">
                                         <span className="label-text">Password</span>
                                     </label>
-                                    <input type="password" placeholder="Enter password" className="input input-bordered" />
+                                    <input type="password" {...register("user password")} placeholder="Enter password" className="input input-bordered" />
                                 </div>
                                 <div className="form-control mt-6">
                                     <button type='submit' className="btn btn-primary">Login</button>
