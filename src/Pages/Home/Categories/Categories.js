da0f8f63fab98282b23a82d0f86ee8011c94237e
AbrRahman <abrahman.est@gmail.com>
2022-11-29 10:55:23 +0600

    loade categorise

--- src/Pages/Home/Categories/Categories.js
+++ src/Pages/Home/Categories/Categories.js
@@ -1,26 +1,20 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import CategoriesCard from './CategoriesCard';
 
 const Categories = () => {
-    const categories = [
-        {
-            _id: 1,
-            name: 'HP Laptops'
-        },
-        {
-            _id: 2,
-            name: 'Dell Laptops'
-        },
-        {
-            _id: 3,
-            name: 'lenovo Laptops'
-        },
-    ]
+    const [categories, setCategories] = useState([])
+
+    useEffect(() => {
+        axios.get('http://localhost:8000/categories')
+            .then(res => setCategories(res.data))
+            .catch(err => console.log(err))
+    }, [])
     return (
         <div className='my-9'>
             <h1 className='text-4xl text-center my-4'>Laptops Categories</h1>
             <div className='grid gap-4 sm:grid-cols-1 md:gird-cols-2 lg:grid-cols-3 my-6'>
-                {categories.map(category => <CategoriesCard
+                {categories?.map(category => <CategoriesCard
                     key={category._id}
                     category={category}
                 ></CategoriesCard>)}
